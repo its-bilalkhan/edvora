84cc091a8f7cd1dcad09c256238e373530fa3672
its-bilalkhan <58695539+its-bilalkhan@users.noreply.github.com>
2022-02-14 15:26:05 +0500

    Delete app/src/main/java/com/edvora/model directory

--- app/src/main/java/com/edvora/model/Paths.kt
+++
@@ -1,9 +0,0 @@
-package com.mrusmansiddique.gpt_android.models
-
-
-class Paths {
-    var pathX: Float = 0.0f
-    var pathY: Float = 0.0f
-    var isActionDown: Boolean = true
-    var color: Int? = 0
-}
